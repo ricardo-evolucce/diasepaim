7e84e1df3e1c70cbb294e7880f27864f3d7453c8
ricardo <ricardo.evolucce@gmail.com>
2022-04-25 16:24:13 -0300

    ajustes

---
+++ includes/menu.php
@@ -0,0 +1,126 @@
+ <header id="header" class="header-transparent header-semi-transparent " data-plugin-options="{'stickyEnabled': true, 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': false, 'stickyChangeLogo': false, 'stickyStartAt': 53, 'stickySetTop': '-53px'}" style="position: relative;">
+    <div class="header-body border-top-0 bg-dark box-shadow-none">
+        <div class="header-top header-top-borders header-top-light-2-borders">
+            <div class="container container-xl-custom h-100">
+                <div class="header-row h-100">
+                    <div class="header-column justify-content-start">
+                        <div class="header-row">
+                            <nav class="header-nav-top">
+                                <ul class="nav nav-pills">
+                                    <li class="nav-item nav-item-borders py-2 d-none d-sm-inline-flex">
+                                        <span class="ps-0"><i class="far fa-dot-circle text-4 text-color-primary" style="top: 1px;"></i>Av. Getúlio Vargas, nº 1151, sala 1016, bairro Menino Deus - Porto Alegre / RS</span>
+                                    </li>
+                                    <li class="nav-item nav-item-borders py-2">
+                                        <a href="#" class="text-color-hover-light"><i class="fab fa-whatsapp text-4 text-color-primary" style="top: 0;"></i>(51) 3516-4188</a>
+                                    </li>
+                                    <li class="nav-item nav-item-borders py-2 d-none d-md-inline-flex">
+                                        <a href="mailto:contato@diasepaimadvogados.com.br" class="text-color-hover-light"><i class="far fa-envelope text-4 text-color-primary" style="top: 1px;"></i>contato@diasepaimadvogados.com.br</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="header-column justify-content-end">
+                        <div class="header-row">
+                            <nav class="header-nav-top">
+                                <ul class="footer-social-icons social-icons social-icons-clean social-icons-big social-icons-opacity-light social-icons-icon-light mt-0 mt-lg-3">
+                                    <li class="social-icons-facebook"><a href="https://www.instagram.com/diasepaimadvogados/" target="_blank" title="Instagram"><i class="fab fa-instagram text-2"></i></a></li>
+                                            <!--<li class="social-icons-twitter"><a href="http://www.twitter.com/" target="_blank" title="Twitter"><i class="fab fa-twitter text-2"></i></a></li>
+                                                <li class="social-icons-linkedin"><a href="http://www.linkedin.com/" target="_blank" title="Linkedin"><i class="fab fa-linkedin-in text-2"></i></a></li>-->
+                                            </ul>
+                                        </nav>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="header-container header-container-height-sm container container-xl-custom">
+                        <div class="header-row">
+                            <div class="header-column">
+                                <div class="header-row">
+                                    <div class="header-logo">
+                                        <a href="index.php">
+                                            <img alt="Dias e Paim"  src="img/logo-novo-2.png">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="header-column justify-content-end">
+                                <div class="header-row">
+                                    <div class="header-nav header-nav-links header-nav-dropdowns-dark header-nav-light-text order-2 order-lg-1">
+                                        <div class="header-nav-main header-nav-main-mobile-dark header-nav-main-square header-nav-main-dropdown-no-borders header-nav-main-effect-2 header-nav-main-sub-effect-1">
+                                            <nav class="collapse">
+                                                <ul class="nav nav-pills" id="mainNav">
+
+
+
+
+
+
+
+                                                    <li>
+                                                        <a class="dropdown-item active" href="index.php">Início</a>
+                                                    </li>
+                                                    <li>
+                                                        <a class="dropdown-item" href="quem-somos.php">Quem somos</a>
+                                                    </li>
+                                                    <li class="dropdown">
+                                                        <a class="dropdown-item dropdown-toggle" href="#">
+                                                            Áreas de Atuação
+                                                        </a>
+                                                        <ul class="dropdown-menu">
+                                                            <li>
+                                                                <a class="dropdown-item" href="direito-do-trabalho-porto-alegre.php">Direito do Trabalho</a>
+                                                            </li>
+                                                            <li>
+                                                                <a class="dropdown-item" href="industria-farmaceutica-porto-alegre.php">Indústria Farmacêutica</a>
+                                                            </li>
+                                                            <li>
+                                                                <a class="dropdown-item" href="direito-previdenciario-porto-alegre.php">Direito Previdenciário</a>
+                                                            </li>
+                                                            <li>
+                                                                <a class="dropdown-item" href="direito-desportivo-porto-alegre.php">Direito Desportivo</a>
+                                                            </li>
+                                                            <li>
+                                                                <a class="dropdown-item" href="atuacao-nos-tribunais-superiores-porto-alegre.php">Atuação nos Tribunais Superiores</a>
+                                                            </li>
+
+                                                        </ul>
+                                                    </li>
+
+                                                    <li class="dropdown">
+                                                        <a class="dropdown-item dropdown-toggle" href="#">
+                                                            Blog
+                                                        </a>
+                                                        <ul class="dropdown-menu">
+
+                                                            <li>
+                                                                <a class="dropdown-item" href="https://blog.diasepaimadvogados.com.br/category/entrevistas/">Entrevistas</a>
+                                                            </li>
+                                                            <li>
+                                                                <a class="dropdown-item" href="https://blog.diasepaimadvogados.com.br/category/palestras-eventos/">Palestras / Eventos</a>
+                                                            </li>
+                                                        </ul>
+                                                    </li>
+
+
+
+
+
+                                                    <li>
+                                                        <a class="dropdown-item" href="contato.php">Contato</a>
+                                                    </li>
+                                                </ul>
+                                            </nav>
+                                        </div>
+                                        <button class="btn header-btn-collapse-nav" data-bs-toggle="collapse" data-bs-target=".header-nav-main nav">
+                                            <i class="fas fa-bars"></i>
+                                        </button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
