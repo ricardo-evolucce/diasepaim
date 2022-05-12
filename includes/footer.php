adee9fdbb2b7f27d83ad80ae796933258d33d64e
ricardo <ricardo.evolucce@gmail.com>
2022-05-12 19:10:28 -0300

    ajutes

---
+++ includes/footer.php
@@ -0,0 +1,48 @@
+        <footer id="footer" class="mt-0 bg-color-light-scale-1 border-0">
+            <div class="container">
+                <div class="row py-5 justify-content-center">
+                    <div class="col-md-9 offset-md-1 offset-lg-0 mb-4 mb-lg-0 d-flex align-items-center">
+                        <div class="footer-nav footer-nav-links footer-nav-bottom-line">
+                            <nav>
+                                <ul class="nav" id="footerNav">
+                                    <li>
+                                        <a href="index.php" class="text-color-dark active">Início</a>
+                                    </li>
+                                    <li>
+                                        <a href="quem-somos.php" class="text-color-dark">Quem somos</a>
+                                    </li>
+                                   
+                                    <li>
+                                        <a href="https://blog.diasepaimadvogados.com.br" class="text-color-dark">Blog</a>
+                                    </li>
+                                    <li>
+                                        <a href="contato.php" class="text-color-dark">Contato</a>
+                                    </li>
+
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-lg-3 mb-4 mb-lg-0 text-center text-lg-end">
+                        <h5 class="text-3 text-color-dark mb-0 pb-1 opacity-6">ENTRE EM CONTATO</h5>
+                        <a href="tel:+1234567890" class="text-8 text-color-dark text-color-hover-primary text-decoration-none font-weight-bold pb-1 mb-0">(51) 3516-4188</a>
+                        <p class="m-0 text-center"><a href="mailto:mail@example.com" class="text-color-dark text-color-hover-primary">
+                        contato@diasepaimadvogados.com.br</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-copyright footer-copyright-style-2 bg-color-light-scale-1 footer-top-border">
+                <div class="container py-2">
+                    <div class="row py-4">
+                        <div class="col-lg-1 d-flex align-items-center justify-content-center justify-content-lg-start mb-2 mb-lg-0">
+                            <a href="#" class="logo pe-0 pe-lg-3">
+                                <img  src="img/logo-novo-2.png" height="32">
+                            </a>
+                        </div>
+                        <div class="col-lg-11 d-flex align-items-center justify-content-center justify-content-lg-end mb-4 mb-lg-0">
+                            <p>Desenvolvido por <a href="http://www.prmuller.com">PRMüller Soluções em Informática</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
