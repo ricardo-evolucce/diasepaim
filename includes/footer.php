dbafeae63bddae0eab1ad94247c590177fdf0380
ricardo <ricardo.evolucce@gmail.com>
2022-06-01 23:15:25 -0300

    ajustes

--- includes/footer.php
+++ includes/footer.php
@@ -46,3 +46,38 @@
                 </div>
             </div>
         </footer>
+
+            </div>
+
+      <link rel="stylesheet" href="css/theme-elements-new.css">
+    
+
+        <link rel="stylesheet" href="css/theme.css">
+
+    <!-- Vendor -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/jquery.appear/jquery.appear.min.js"></script>
+    <script src="vendor/jquery.easing/jquery.easing.min.js"></script>
+    <script src="vendor/jquery.cookie/jquery.cookie.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/jquery.validation/jquery.validate.min.js"></script>
+    <script src="vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
+    <script src="vendor/jquery.gmap/jquery.gmap.min.js"></script>
+    <script src="vendor/lazysizes/lazysizes.min.js"></script>
+    <script src="vendor/isotope/jquery.isotope.min.js"></script>
+    <script src="vendor/owl.carousel/owl.carousel.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+    <script src="vendor/vide/jquery.vide.min.js"></script>
+    <script src="vendor/vivus/vivus.min.js"></script>
+
+    <!-- Theme Base, Components and Settings -->
+    <script src="js/theme.js"></script>
+
+
+    <!-- Theme Custom -->
+    <script src="js/custom.js"></script>
+
+    <!-- Theme Initialization Files -->
+    <script src="js/theme.init.js"></script>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
---
+++ includes/head.php
@@ -0,0 +1,33 @@
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="img/favicon.jpg" type="image/x-icon" />
+  <link rel="apple-touch-icon" href="img/apple-touch-icon2.png">
+
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+
+  <!-- Web Fonts  
+  <link id="googleFonts" href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800%7CShadows+Into+Light&display=swap" rel="stylesheet" type="text/css">
+-->
+  <!-- Vendor CSS -->
+  <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="vendor/animate/animate.compat.css">
+  <!--<link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.min.css">-->
+  <link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.min.css">
+  <link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.min.css">
+  <link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.min.css">
+
+  <!-- Theme CSS -->
+
+
+  <link rel="stylesheet" href="css/theme-blog.css">
+  <!--<link rel="stylesheet" href="css/theme-shop.css">-->
+
+  <!-- Skin CSS -->
+  <link id="skinCSS" rel="stylesheet" href="css/skins/skin-corporate-21.css">
+
+  <!-- Theme Custom CSS -->
+  <link rel="stylesheet" href="css/custom.css">
+
+  <!-- Head Libs -->
+  <script src="vendor/modernizr/modernizr.min.js"></script>
